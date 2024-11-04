8f93e2969bf9383d6aad8cfcaec0ae00084ce786
calebcordier <37548838+calebcordier@users.noreply.github.com>
2024-11-04 11:14:31 -0500

    Update fitness-planner tables.sql to account for tracking completed workouts

--- database/fitness-planner tables.sql
+++ database/fitness-planner tables.sql
@@ -85,8 +85,10 @@ CREATE TABLE Notification (
 CREATE TABLE Progress_Analytics (
     analytics_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
-    workout_data TEXT,
+    fitness_goals TEXT,
     progress_data TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES User(user_id)
+    FOREIGN KEY (fitness_goals) REFERENCES Workout_Plan(fitness_goals)
 );
+
